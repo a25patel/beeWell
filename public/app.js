1def8f75a5ccb3fbbc1b9c1e664afa6ee98e0b99
a25patel <aanaletap2510@gmail.com>
2015-10-12 12:00:31 -0600

    angular routes

--- public/app.js
+++ public/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('beeWell', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('vaccines', {
+      url: '/',
+      templateUrl: 'partials/vaccines.html',
+      controller: 'vaccineController'
+    })
+});
